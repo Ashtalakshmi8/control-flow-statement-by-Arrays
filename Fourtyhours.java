321ebc7c9e28a11d0cce9d93117caddc696218f9
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-27 12:55:45 +0530

    Arrays-Week-Month

---
+++ Fourtyhours.java
@@ -0,0 +1,8 @@
+class Fourtyhours{
+public static void main (String [] args){ 
+String []week={"sunday", "monday","tuesday","wednesday","thursday","friday","saturday"};
+for(int index=0;index<week.length;index=index+1){
+System.out.println(week[index]);
+}
+}
+}
---
+++ Months.java
@@ -0,0 +1,8 @@
+class Months{
+public static void main (String [] args){
+String[]Months={"January","Febuary","March","April","May","June","July","August","September","October","November","December"};
+for (int index=0;index < Months.length;index=index+1){
+System.out.println(Months[index]);
+}
+}
+}
